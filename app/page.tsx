56f00bb0cb9b968772bd455133caa15e9dc8706d
The Hacking Alien <marksyapoe@gmail.com>
2023-12-27 16:40:25 +0100

    'finished'

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,17 @@ export default function Home() {
       <div className='fixed w-full bg-slate-900 h-full bg-opacity-60'>
         <h1 className='text-center text-5xl mt-28 font-semibold'>Sage Tek Club</h1>
         
-        <p className='text-center mt-20 text-xl'>Welcome to, SageTek opening.<br></br> Join us in building and growing as cyber elites.<br></br>Apply below to one of our two paths,<br></br>The way of the warrior and the way of the creator.</p>
+        <p className='text-center mt-20 ml-4 mr-4 text-xl'>Welcome to, SageTek opening.<br></br> Join us in building and growing as cyber elites.<br></br>Apply below to one of our two paths,<br></br>The way of the warrior and the way of the creator.</p>
 
-        <div className='mt-20 w-full sm:flex sm:justify-center text-lg font-semibold ml-8 sm:ml-0'>
+        
+
+        <div className='mt-20 w-full grid grid-cols-1 justify-items-center md:flex md:justify-center text-lg font-semibold'>
           <button className='w-[15rem] h-12 rounded-md bg-white text-slate-900 hover:bg-slate-700 hover:text-white group'><p className='group-hover:animate-bounce'>Cyber Warrior- Hacker</p></button>
 
-          <button className='w-[15rem] h-12 rounded-md mt-4 sm:mt-0 bg-white text-slate-900 sm:ml-20 hover:bg-slate-700 hover:text-white group'><p className='group-hover:animate-bounce'>Cyber Creator- Developer</p></button>
+          <button className='w-[15rem] h-12 rounded-md mt-4 md:mt-0 bg-white text-slate-900 md:ml-20 hover:bg-slate-700 hover:text-white group'><p className='group-hover:animate-bounce'>Cyber Creator- Developer</p></button>
         </div>
+
+        
         
       </div>
     </main>
